d638bd594e27a7a4c9b2975ddf0bc125ea2b33b5
Dipanjan Pradhan <dipayanpradhan637@gmail.com>
2026-08-24 12:16:21 +0530

    Initial commit

---
+++ Starprint.js
@@ -0,0 +1,34 @@
+for (let i = 1; i <= 5; i++) {
+    let star = "";
+
+    for (let j = 1; j <= i; j++) {
+        star += "* ";
+    }
+
+    console.log(star);
+}
+console.log(" ")
+for (let i = 1; i <= 5; i++) {
+        let star = "";
+    for (let j = 1; j <= 5; j++) {
+        star += "* ";
+    }
+
+    console.log(star);
+}
+console.log(" ")
+let i, j, k;
+
+for (i = 1; i <= 5; i++) {
+    let str = "";
+
+    for (j = 1; j <= 5 - i; j++) {
+        str += " ";
+    }
+
+    for (k = 1; k <= i; k++) {
+        str += "* ";
+    }
+
+    console.log(str);
+}
