b5ce86a96ea2666595379ca4c53045495515d72b
Dipanjan Pradhan <dipayanpradhan637@gmail.com>
2026-08-25 12:47:15 +0530

    add

--- Starprint.js
+++ Starprint.js
@@ -50,3 +50,60 @@ for (s = 1; s<= 9; s++) {
     console.log(str);
 }
 console.log(" ")
+let n = 5;
+let pattern = "";
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i; j++) {
+        pattern += " ";
+    }
+
+    for (let j = 1; j <= 2 * i - 1; j++) {
+        pattern += "*";
+    }
+
+    pattern += "\n";
+}
+
+for (let i = n - 1; i >= 1; i--) {
+    for (let j = 1; j <= n - i; j++) {
+        pattern += " ";
+    }
+
+    for (let j = 1; j <= 2 * i - 1; j++) {
+        pattern += "*";
+    }
+
+    pattern += "\n";
+}
+
+console.log(pattern);
+console.log(" ")
+let rows1 = 5;
+let result1 = "";
+
+for (let i = 1; i <= rows1; i++) {
+    for (let j = 1; j <= i; j++) {
+        result1 += "* ";
+    }
+    result1 += "\n";
+}
+
+console.log(result1);
+console.log(" ")
+let rows4 = 5;
+let result4 = "";
+
+for (let i = 3; i <= rows4; i++) {
+    for (let j = 1; j <= rows4 - i; j++) {
+        result4 += " ";
+    }
+
+    for (let j = 1; j <= 4 * i - 2; j++) {
+        result4 += "*";
+    }
+
+    result4 += "\n";
+}
+
+console.log(result4);
